6e46c4a2e991329d59f27d25528c6934bd8f0372
Sandis Sticers <sandis.sticers@fjordsoftware.eu>
2017-12-28 02:51:30 +0200

    shortened embed command for rs_item

--- bot/bot.rb
+++ bot/bot.rb
@@ -5,7 +5,8 @@ require_all 'decorators/embeds'
 bot = Discordrb::Bot.new token: Config.new.secrets.discord.token
 
 bot.message(with_text: 'Ping!') do |event|
-  
+  event.channel.send_embed('', RsItem.find(1077).ge_embed)
+  binding.pry
   event.respond 'Pong!'
 end
 
--- bot/models/rs_item.rb
+++ bot/models/rs_item.rb
@@ -1,8 +1,14 @@
 require_relative 'application_record'
 require_relative '../config/rs3'
+require_all 'decorators/embeds'
+
 class RsItem < ApplicationRecord
   def ge
     uri = URI(RS3::GE_JSON + id.to_s)
     json = JSON.parse(Net::HTTP.get(uri))
+  end
+
+  def ge_embed
+    Decorators::Embeds::GeInfo.new(self).embed
   end
 end
